233b60f5fc98009dd13b0f538b8c60b4c0a087d2
vicsenedesse <engsoftvicsenedesse@gmail.com>
2021-03-20 21:02:53 -0400

    Commitando para fazer merge

--- dist/cats/cats.controller.d.ts
+++ dist/cats/cats.controller.d.ts
@@ -1,3 +1,8 @@
 export declare class CatsController {
-    findAll(): string;
+    creat(): string;
+    create(_createCatDto: any): string;
+    findAll(query: any): string;
+    findOne(id: any): string;
+    update(id: any, _updateCatDto: any): string;
+    remove(id: any): string;
 }
